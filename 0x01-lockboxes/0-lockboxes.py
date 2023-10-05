2660bad4e995961d3184114589ded2f06f32044d
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-05 09:39:33 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Returns true or false
+    """
+    opened = set()
+    queue = [0]
+
+    while queue:
+        current = queue.pop(0)
+        #print(current)
+        opened.add(current)
+        print(opened)
+
+        queue.extend(key for key in boxes[current] if key not in opened and key < len(boxes))
+    
+    return len(opened) == len(boxes)
