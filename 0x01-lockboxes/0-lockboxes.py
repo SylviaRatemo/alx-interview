e288e800c483ac89a9e084258445bae3e81a9e17
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-05 10:31:28 +0300

    req errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,16 +6,15 @@ Lockboxes
 
 def canUnlockAll(boxes):
     """
-    Returns true or false
+    #Returns true or false
     """
     opened = set()
     queue = [0]
 
     while queue:
         current = queue.pop(0)
-        #print(current)
         opened.add(current)
 
-        queue.extend(key for key in boxes[current] if key not in opened and key < len(boxes))
-    
+        queue.extend(key for key in boxes[current]
+                     if key not in opened and key < len(boxes))
     return len(opened) == len(boxes)
