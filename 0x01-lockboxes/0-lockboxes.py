e1c8703bcce1cf3bd4c1ecbf3d8dd9f6213fe892
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-05 20:25:35 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,22 +2,28 @@
 """
 Lockboxes
 """
+from collections import deque
 
 
 def canUnlockAll(boxes):
     """
     Returns True if all boxes can be opened, else returns False.
     """
+    n = len(boxes)
     opened = set()
-    queue = [0]
+    queue = deque([0])
 
     while queue:
-        current = queue.pop(0)
+        current = queue.popleft()
         opened.add(current)
 
         unseen_boxes = set(boxes[current]) - opened
-        valid_keys = [key for key in unseen_boxes if 0 <= key < len(boxes)]
+        valid_keys = [key for key in unseen_boxes if 0 <= key < n]
 
         queue.extend(valid_keys)
 
-    return len(opened) == len(boxes)
+        # Check if all boxes are opened inside the loop
+        if len(opened) == n:
+            return True
+
+    return False
