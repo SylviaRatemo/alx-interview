01dad1d01037178546d7c2b1a77d01f13c3e9263
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-19 14:40:17 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,45 +2,48 @@
 '''Log Parsing
 '''
 import sys
-stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
-summ = 0
 
+STATUS_CODES = {"200", "301", "400", "401", "403", "404", "405", "500"}
+stats = {code: 0 for code in STATUS_CODES}
+total_size = 0
 
-def prn_stats():
+def print_stats():
     """
-    Function that print stats about log
+    Function that prints stats about log
     """
-    global summ
-
-    print('File size: {}'.format(summ))
-    stcdor = sorted(stcd.keys())
-    for each in stcdor:
-        if stcd[each] > 0:
-            print('{}: {}'.format(each, stcd[each]))
-
+    print(f'File size: {total_size}')
+    for code, count in sorted(stats.items()):
+        if count > 0:
+            print(f'{code}: {count}')
 
 if __name__ == "__main__":
-    cnt = 0
+    line_count = 0
+
     try:
-        """ Iter the standar input """
-        for data in sys.stdin:
+        """ Iterate over standard input """
+        for line in sys.stdin:
             try:
-                fact = data.split(' ')
+                fields = line.split()
+                status_code = fields[-2]
+
                 """ If there is a status code """
-                if fact[-2] in stcd:
-                    stcd[fact[-2]] += 1
-                """ If there is a lenght """
-                summ += int(fact[-1])
-            except:
+                if status_code in STATUS_CODES:
+                    stats[status_code] += 1
+
+                """ If there is a length """
+                total_size += int(fields[-1])
+
+            except (ValueError, IndexError):
                 pass
+
             """ Printing control """
-            cnt += 1
-            if cnt == 10:
-                prn_stats()
-                cnt = 0
+            line_count += 1
+            if line_count == 10:
+                print_stats()
+                line_count = 0
+
     except KeyboardInterrupt:
-        prn_stats()
+        print_stats()
         raise
     else:
-        prn_stats()
+        print_stats()
