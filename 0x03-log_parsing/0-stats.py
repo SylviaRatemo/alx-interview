ed97d0d1221ef11baf2f929bf3915e12d89503e1
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-19 14:38:05 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,54 +2,45 @@
 '''Log Parsing
 '''
 import sys
-import re
-from collections import defaultdict
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def process_line(line, metrics):
-    # Regular expression pattern for matching the input format
-    pattern = re.compile(
-        r'(\d+\.\d+\.\d+\.\d+) - \[.*\] "GET /projects/260 HTTP/1.1" '
-        r'(\d+) (\d+)'
-    )
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
 
-    match = pattern.match(line)
-    if match:
-        ip_address, status_code, file_size = match.groups()
-        valid = {'200', '301', '400', '401', '403', '404', '405', '500'}
-        if status_code in valid:
-            metrics['total_size'] += int(file_size)
-            metrics['status_codes'][status_code] += 1
-
-
-def print_metrics(metrics):
-    print(f"File size: {metrics['total_size']}")
-
-    for code in sorted(metrics['status_codes']):
-        count = metrics['status_codes'][code]
-        print(f"{code}: {count}")
-
-
-def main():
-    metrics = {
-        'total_size': 0,
-        'status_codes': defaultdict(int)
-    }
-
-    try:
-        for i, line in enumerate(sys.stdin, start=1):
-            process_line(line.strip(), metrics)
-
-            if i % 10 == 0:
-                print_metrics(metrics)
-
-    except KeyboardInterrupt:
-        '''Handle KeyboardInterrupt (CTRL + C)
-        '''
-        print_metrics(metrics)
-        sys.stderr.write("KeyboardInterrupt\n")
-        sys.exit(0)
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
 if __name__ == "__main__":
-    main()
+    cnt = 0
+    try:
+        """ Iter the standar input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
+            except:
+                pass
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
+    except KeyboardInterrupt:
+        prn_stats()
+        raise
+    else:
+        prn_stats()
