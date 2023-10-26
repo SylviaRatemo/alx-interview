183b9d6c2624ed1087113ac962184f74a3c88490
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-26 10:41:34 +0300

    UTF-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    count = 0
+    for x in data:
+        if count == 0:
+            if (x >> 5) == 0b110:
+                count = 1
+            elif (x >> 4) == 0b1110:
+                count = 2
+            elif (x >> 3) == 0b11110:
+                count = 3
+            elif (x >> 7) != 0:
+                return False
+        else:
+            if (x >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
