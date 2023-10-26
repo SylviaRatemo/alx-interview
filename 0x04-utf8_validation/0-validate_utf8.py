058471085a11029f2cb9d25284f1f0590070ef23
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-26 11:21:40 +0300

    UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
-""" UTF-8 Validation
 """
-
-# Constants for binary literals
-TWO_BYTE_MASK = 0b110
-THREE_BYTE_MASK = 0b1110
-FOUR_BYTE_MASK = 0b11110
-CONTINUATION_MASK = 0b10
+Method that determines if a given data set represents a valid UTF-8 encoding.
+"""
 
 
 def validUTF8(data):
-    """Check if the given list of integers represents a valid UTF-8 encoding.
-
-    Args:
-        data (list): List of integers representing UTF-8 encoded characters.
-
-    Returns:
-        bool: True if valid UTF-8 encoding, False otherwise.
-    """
-    count = 0
-    for x in data:
-        if count == 0:
-            if (x >> 5) == TWO_BYTE_MASK:
-                count = 1
-            elif (x >> 4) == THREE_BYTE_MASK:
-                count = 2
-            elif (x >> 3) == FOUR_BYTE_MASK:
-                count = 3
-            elif (x >> 7) != 0:
+    """ UTF-8 Validation """
+    bytesLong = 0
+    binaryMoveLeft7 = 1 << 7
+    binaryMoveLeft6 = 1 << 6
+    for byte in data:
+        binaryMove = 1 << 7
+        if bytesLong == 0:
+            while byte & binaryMove:
+                bytesLong += 1
+                binaryMove = binaryMove >> 1
+            if bytesLong == 0:
+                continue
+            if bytesLong == 1 or bytesLong > 4:
                 return False
         else:
-            if (x >> 6) != CONTINUATION_MASK:
+            if not (byte & binaryMoveLeft7 and not (byte & binaryMoveLeft6)):
                 return False
-            count -= 1
-    return count == 0
+        bytesLong -= 1
+    if bytesLong == 0:
+        return True
+    return False
