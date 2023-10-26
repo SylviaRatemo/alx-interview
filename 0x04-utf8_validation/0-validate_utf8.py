fa2e095e2395c556293d1c5c15b01a47ef754584
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-26 10:43:36 +0300

    UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,21 +2,36 @@
 """ UTF-8 Validation
 """
 
+# Constants for binary literals
+TWO_BYTE_MASK = 0b110
+THREE_BYTE_MASK = 0b1110
+FOUR_BYTE_MASK = 0b11110
+CONTINUATION_MASK = 0b10
+HIGH_BIT_MASK = 0b10000000
+
 
 def validUTF8(data):
+    """Check if the given list of integers represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): List of integers representing UTF-8 encoded characters.
+
+    Returns:
+        bool: True if valid UTF-8 encoding, False otherwise.
+    """
     count = 0
     for x in data:
         if count == 0:
-            if (x >> 5) == 0b110:
+            if (x >> 5) == TWO_BYTE_MASK:
                 count = 1
-            elif (x >> 4) == 0b1110:
+            elif (x >> 4) == THREE_BYTE_MASK:
                 count = 2
-            elif (x >> 3) == 0b11110:
+            elif (x >> 3) == FOUR_BYTE_MASK:
                 count = 3
             elif (x >> 7) != 0:
                 return False
         else:
-            if (x >> 6) != 0b10:
+            if (x >> 6) != CONTINUATION_MASK:
                 return False
             count -= 1
     return count == 0
