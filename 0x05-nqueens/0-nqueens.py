898d2453c864aca7d27acd4ef417de807f2bc55e
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-03 05:38:23 +0300

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,7 +5,6 @@ N queens
 import sys
 
 
-
 def is_safe(board, row, col, n):
     # Check if there is a queen in the same column
     for i in range(row):
@@ -14,6 +13,7 @@ def is_safe(board, row, col, n):
            board[i] + i == col + row:
             return False
     return True
+
 
 def solve_nqueens(board, row, n):
     if row == n:
@@ -24,6 +24,7 @@ def solve_nqueens(board, row, n):
         if is_safe(board, row, col, n):
             board[row] = col
             solve_nqueens(board, row + 1, n)
+
 
 def nqueens(n):
     if not n.isdigit():
@@ -37,6 +38,7 @@ def nqueens(n):
 
     board = [-1] * n
     solve_nqueens(board, 0, n)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
