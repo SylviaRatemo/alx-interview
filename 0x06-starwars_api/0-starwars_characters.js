88cd47ab7f5e41202b0da57282c4f4e6b35711e4
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-09 07:34:25 +0300

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -24,36 +24,18 @@ request(apiUrl, (error, response, body) => {
       console.log('No characters found for this movie.');
     } else {
       console.log(`Characters in ${film.title}:`);
-      const sortedCharacters = [];
-
-      // Use a recursive function to handle asynchronous character requests
-      function getCharacter(index) {
-        if (index === characters.length) {
-          // All characters fetched, now sort and print
-          sortedCharacters.sort();
-          sortedCharacters.forEach((characterName) => {
-            console.log(characterName);
-          });
-        } else {
-          // Fetch character and add to sorted list
-          const characterUrl = characters[index];
-          request(characterUrl, (charError, charResponse, charBody) => {
-            if (charError) {
-              console.error('Error fetching character:', charError);
-            } else if (charResponse.statusCode !== 200) {
-              console.error('Unexpected status code for character:', charResponse.statusCode);
-            } else {
-              const character = JSON.parse(charBody);
-              sortedCharacters.push(character.name);
-              // Continue to the next character
-              getCharacter(index + 1);
-            }
-          });
-        }
-      }
-
-      // Start fetching characters
-      getCharacter(0);
+      characters.forEach((characterUrl) => {
+        request(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error('Error fetching character:', charError);
+          } else if (charResponse.statusCode !== 200) {
+            console.error('Unexpected status code for character:', charResponse.statusCode);
+          } else {
+            const character = JSON.parse(charBody);
+            console.log(character.name);
+          }
+        });
+      });
     }
   }
 });
