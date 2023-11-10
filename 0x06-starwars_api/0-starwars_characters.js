dbb320cf6d56094401ba2bdbaed120efeb6e7504
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-10 04:15:32 +0300

    StarWars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a Movie ID as a command line argument.');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-  } else {
-    const film = JSON.parse(body);
-    const characters = film.characters;
-
-    if (characters.length === 0) {
-      console.log('No characters found for this movie.');
-    } else {
-      console.log(`Characters in ${film.title}:`);
-      characters.forEach((characterUrl) => {
-        request(characterUrl, (charError, charResponse, charBody) => {
-          if (charError) {
-            console.error('Error fetching character:', charError);
-          } else if (charResponse.statusCode !== 200) {
-            console.error('Unexpected status code for character:', charResponse.statusCode);
-          } else {
-            const character = JSON.parse(charBody);
-            console.log(character.name);
-          }
-        });
-      });
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-  }
-});
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
