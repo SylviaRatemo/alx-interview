1e52bea699d499c2b7b65a92355b505448be8219
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-24 14:50:07 +0300

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""
-Making Changes
-"""
+"""Making change O(n)"""
 
 
 def makeChange(coins, total):
-    """
-    Find the fewest number of coins needed to meet a given amount total.
-    """
-    if total <= 0:
+    """Clasic Bottom-Up dynamic programming"""
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
 
-    # Base case: 0 coins needed for a total of 0
-    dp[0] = 0
-
-    # Update the dp array for each total from 1 to the target total
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-
-    return dp[total]
+    return temp_value if total == 0 else -1
